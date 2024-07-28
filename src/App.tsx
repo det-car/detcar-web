84ea7b5cfdc19a57824ae6dfa1a093f4bb9164a5
daenikon <dkondratiuk06@gmail.com>
2024-07-28 21:32:23 +0200

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/sections/header/Header";
 import ServicesSection from "./components/sections/services/ServicesSection";
 import ContactSection from "./components/sections/contact/ContactSection";
 import Footer from "./components/sections/footer/Footer";
 import Topbar from "./components/sections/header/Topbar";
-import Kontakt from "./components/pages/Kontakt"; // Import Kontakt page
+import Kontakt from "./components/pages/Kontakt";
 import "./App.css";
 
 function App() {
