93fb6c9d6438fee51e018571ff0b557167b5422f
daenikon <dkondratiuk06@gmail.com>
2024-08-05 20:12:51 +0200

    add hero text

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -12,9 +12,13 @@ const Hero: React.FC = () => {
       />
       <div className="absolute inset-0 bg-black opacity-70"></div>
       <div className="relative z-10 flex h-full flex-col items-center justify-center p-4 text-center text-white">
-        <h1 className="mb-4 text-4xl font-bold md:text-6xl">Thesis</h1>
-        <p className="mb-24 text-lg md:text-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <h1 className="mb-4 mt-36 text-3xl font-bold md:text-4xl">
+          Perfektion im Detail
+        </h1>
+        <p className="mb-24 max-w-[800px] text-lg md:text-2xl">
+          Ihr Auto verdient nur das Beste. Wir sind spezialisiert darauf, das
+          Beste aus Ihrem Fahrzeug herauszuholen und es in neuem Glanz
+          erstrahlen zu lassen.
         </p>
         <ContactPopover />
       </div>
